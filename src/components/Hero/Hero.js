9934f874eff91e2e6bb7efc3c92eda6f073b86f3
Bhumika Narale <ghbhumikanarale@gmail.com>
2023-07-20 13:06:11 +0530

    commit for nextjs basic

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </SectionText>
+      <Button onClick ={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
